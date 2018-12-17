02912665dadfb4fea70593a023b926dec78e6a53
kkjang <kevinjang7@gmail.com>
2018-12-16 22:21:31 -0800

    Update string_permutations.py

--- string_permutations.py
+++ string_permutations.py
@@ -10,4 +10,14 @@ def string_permutations(string_arr, begin=None):
         if i != begin:
             string_arr[i], string_arr[begin] = string_arr[begin], string_arr[i]
 
-string_permutations(list("abcd"))
+def string_permutations_in_order(string_arr, prefix=""):
+    if not string_arr:
+        print(prefix)
+    for i, val in enumerate(string_arr):
+        new_prefix = prefix + val
+        new_string_arr = string_arr[:i] + string_arr[i+1:]
+        string_permutations_in_order(new_string_arr, new_prefix)
+
+test_case = list('abcd')
+string_permutations(test_case)
+string_permutations_in_order(test_case)
